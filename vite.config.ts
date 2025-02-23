08cdaca22c22fcd6c842f6e905c25125470b9bd0
sethgabrielbrady <sethgabrielbrady@gmail.com>
2025-02-22 20:50:24 -0500

    add env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vite/client" />
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -5,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sethgabrielbrady/',
+  base: import.meta.env.VITE_BASE_URL,
   plugins: [
     vue(),
     vueDevTools(),
